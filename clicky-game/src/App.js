5c6f491d64332327261698f7c51d86dd05bbb8a3
markeezy <markkhoo95@gmail.com>
2021-05-16 23:51:36 -0700

    randomize cards

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -5,11 +5,30 @@ import images from "./images.json";
 
 class App extends Component {
   state = {
-    images: images,
+    images: this.shuffle(images),
     choosen: [],
     score: 0,
     topScore: 0,
   };
+
+  shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+  
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+  
+    return array;
+  }
 
   render() {
     return (
